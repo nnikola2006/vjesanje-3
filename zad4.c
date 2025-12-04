cb5619ec3cd02005be620309df3b47f6bb241463
Dzonex <89349082+nnikola2006@users.noreply.github.com>
2025-12-04 01:05:19 +0100

    Provjera uslova

--- zad4.c
+++ zad4.c
@@ -10,31 +10,39 @@ int main()
     struct RAZLOMAK a, b, c; // a = prvi, b = drugi, c = rezultat
 
     // Ucitavanje dva razlomka
-    scanf("%d %d", &a.br, &a.im); // unos prvog razlomka
-    scanf("%d %d", &b.br, &b.im); // unos drugog razlomka
+    do 
+    {
+        printf("Unesite prvi razlomak (brojilac >0, imenilac !=0): ");
+        scanf("%d %d", &a.br, &a.im);
+    } while (a.br <= 0 || a.im == 0);
+    do 
+    {
+        printf("Unesite drugi razlomak (brojilac >0, imenilac !=0): ");
+        scanf("%d %d", &b.br, &b.im);
+    } while (b.br <= 0 || b.im == 0);
 
     // Sabiranje: a.br/a.im + b.br/b.im
     c.br = a.br * b.im + b.br * a.im; // brojilac rezultata
     c.im = a.im * b.im;               // imenilac rezultata
 
     // Kracenje razlomka
-    int brojilacZaGCD = c.br;  
-    int imenilacZaGCD = c.im;  
+    int brojilacZaNZD = c.br;  
+    int imenilacZaNZD = c.im;  
     int ostatak;               
-    int gcd;                  
+    int NZD;                  
 
     // Euklidov algoritam (neki baja)
-    while(imenilacZaGCD != 0) 
+    while(imenilacZaNZD != 0) 
     {
-        ostatak = brojilacZaGCD % imenilacZaGCD;
-        brojilacZaGCD = imenilacZaGCD;
-        imenilacZaGCD = ostatak;
+        ostatak = brojilacZaNZD % imenilacZaNZD;
+        brojilacZaNZD = imenilacZaNZD;
+        imenilacZaNZD = ostatak;
     }
-    gcd = brojilacZaGCD; // najveci zajednicki djelilac
+    NZD = brojilacZaNZD; // najveci zajednicki djelilac
 
     // Skrati razlomak
-    c.br /= gcd;
-    c.im /= gcd;
+    c.br /= NZD;
+    c.im /= NZD;
 
     // Ispis
     printf("%d %d\n", c.br, c.im);
